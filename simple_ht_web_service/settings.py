243a00c2c3abfa2842e739917896eebd387d3853
Никита Ангелов <angelov_ni@it-thematic.ru>
2018-04-02 18:34:52 +0300

    set db url

--- simple_ht_web_service/settings.py
+++ simple_ht_web_service/settings.py
@@ -87,7 +87,9 @@ WSGI_APPLICATION = 'simple_ht_web_service.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(
+        default="postgres://qrqfwwuelwmvjc:d1a6465b6db0c1a3d812d66cfcd16a978de7954cdaaa4e03beb96e9b42478790@ec2-174-129-206-173.compute-1.amazonaws.com:5432/d41ttf6tp7or4g"
+    )
 }
 
 # DATABASES = {
